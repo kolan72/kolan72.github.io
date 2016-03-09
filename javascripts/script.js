b082c6e41cade0fa9d4ead9016ba7117c97b3a2b
kolan72 <kolan72@gmail.com>
2016-03-09 15:52:31 +0300

    try to fix long main state page refresh by hiding content

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,15 +2,34 @@
 
 	var app = angular.module("routedTabs", ["ui.router", "ui.bootstrap"]);
 
+	app.run(function ($templateCache, $http) {
+	    $http.get('tab1.html', { cache: true });
+	});
+
 	app.config(function ($stateProvider, $urlRouterProvider, $provide) {
 
 	    $urlRouterProvider.otherwise("/main/tmfexplorer");
-	    //$urlRouterProvider.otherwise("/screens/tmfexplorer");
 	    $stateProvider
-			    .state("main", { abtract: true, url:"/main", templateUrl:"main.html" })
-				.state("main.tab1", { url: "/tmfexplorer", templateUrl: "tab1.html" })
+			    .state("main", {
+			        abtract: true,
+			        url: "/main",
+			        templateUrl: 'main.html',
+
+
+			    })
+				.state("main.tab1", {
+				    url: "/tmfexplorer"
+
+				    
+				    ,templateUrl: "tab1.html"
+
+				})
 				.state("main.tab2", { url: "/wordsbrowser", templateUrl: "tab2.html" })
-                .state("screens", { abtract: true, url: "/screens", templateUrl: "main.html" })
+                .state("screens", {
+                    abtract: true, url: "/screens",
+                    templateUrl: "main.html"
+
+                })
                 .state("screens.tab1", { url: "/tmfexplorer", templateUrl: "tab1screen.html" })
                 .state("screens.tab2", { url: "/wordsbrowser", templateUrl: "tab2screen.html" })
 	    ;
@@ -22,82 +41,76 @@
 	    });
 	})
 
+	app.factory('getStateNameFactory', function () {
+	    return {getStateName : function (name) {
+	        var stateRootName = name;
+	        var dotPos = stateRootName.indexOf(".");
+	        if (dotPos != -1) {
+	            stateRootName = stateRootName.substring(0, dotPos);
+	        }
+	        return stateRootName;
+	    }
+	  };
+	}
+	);
 
-    //.run(['$rootScope', '$state', function ($rootScope, $state) {
-    //    //if (toState.url == '/main') {
-    //    //    event.preventDefault();
-    //    //}
-    //}])
-    ;
-    
-
-	//app.filter('startsWithLetter', function () {
-	//    return function (items, letter) {
-	//        var filtered = [];
-
-	//        for (var i = 0; i < items.length; i++) {
-	//            var item = items[i];
-	//            if (item.route.substring(0, 4) == (letter == '' ? 'main' : letter))
-	//            {
-	//                filtered.push(item);
-	//            }
-	//        }
-	//        return filtered;
-	//    };
-	//});
-
-	app.controller("mainController", function($rootScope, $scope, $state) {		
-
-	    $scope.letter = $state.$current.name;
+	app.controller("mainController", ['$rootScope', '$scope', '$state', '$timeout', 'getStateNameFactory', function ($rootScope, $scope, $state, $timeout, getStateNameFactory) {
 
 	    $scope.go = function (route) {
 	        $state.go(route);
+	        $timeout(
+                function () {
+                    angular.element('#content').css('display', 'block');
+                }, 10);
 
-		};
+	    };
 
-		$scope.active = function(route){
-			return $state.is(route);
-		};
+	    $scope.active = function (route) {
+	        return $state.is(route);
+	    };
 
-		$scope.tabs = [
+	    $scope.tabs = [
 			{ heading: "TMFExplorer", route: "main.tab1", active: false, visible: true },
 			{ heading: "WordsBrowser", route: "main.tab2", active: false, visible: true },
 		    { heading: "TMFExplorer", route: "screens.tab1", active: false, visible: true },
 		    { heading: "WordsBrowser", route: "screens.tab2", active: false, visible: true }
 
-			//{ heading: "Tab 3", route:"main.tab3", active:false },
-		];
+	    ];
 
-		$scope.$on("$stateChangeSuccess", function () {
-			$scope.tabs.forEach(function(tab) {
-				tab.active = $scope.active(tab.route);
-			});
-			var stateRootName = $state.$current.name;
-			var dotPos = $state.$current.name.indexOf(".");
-			if (dotPos != -1)
-			{
-			    stateRootName = stateRootName.substring(0, dotPos);
-			}
-			$scope.tabs.forEach(function (tab) {
-			    tab.visible = !(tab.route.indexOf(stateRootName) == -1);
-			    //console.log($state.$current.name);
-			    //if (!tab.active) {
-			    //    tab.visible = false;
-			    //}
-			});
-		});
+	    $scope.$on("$stateChangeSuccess", function () {
+	        $scope.tabs.forEach(function (tab) {
+	            tab.active = $scope.active(tab.route);
+	        });
+	        var stateRootName = getStateNameFactory.getStateName($state.$current.name);
 
-		$rootScope.$on('$stateChangeStart',
-                        function (event, toState, toParams, fromState, fromParams, options)
-                        {
+	        $scope.tabs.forEach(function (tab) {
+	            tab.visible = !(tab.route.indexOf(stateRootName) == -1);
+
+	        });
+	    });
+	    $rootScope.$on('$stateNotFound', function (event, unfoundState, fromState, fromParams) {
+	        console.log(unfoundState.to);
+	    }
+        );
+
+	    $rootScope.$on('$stateChangeError',
+            function (event, toState, toParams, fromState, fromParams, error) {
+                console.log(toState.url);
+            }
+    );
+
+	    $rootScope.$on('$stateChangeStart',
+                        function (event, toState, toParams, fromState, fromParams, options) {
                             if (toState.url == '/main' || toState.url == '/screens') {
                                 event.preventDefault();
                             }
-                            else
-                            {
-                                ;
+                            else {
+                                if (!(getStateNameFactory.getStateName(fromState.name) == getStateNameFactory.getStateName(toState.name)))
+                                {
+                                    angular.element('#content').css('display', 'none');
+                                }
                             }
                         })
-	});
+	}]);
 
 }());
